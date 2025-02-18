7441c210928a71332ad4afe4be0faf0d3afc26cf
KAUAN DE ASSIS LEONEL RODRIGUES <24122535@dsn127m1242375.cfp127aluno.local>
2025-02-18 17:17:01 -0300

    Tela home concluida

---
+++ app/src/main/java/sp/senai/jandira/bmi/MainActivity.kt
@@ -0,0 +1,21 @@
+package sp.senai.jandira.bmi
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import sp.senai.jandira.bmi.screens.HomeScreen
+import sp.senai.jandira.bmi.ui.theme.BMITheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BMITheme {
+               HomeScreen()
+            }
+        }
+    }
+}
+
